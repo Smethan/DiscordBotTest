d0f73cafbbc7ee04995774747b8fae2fb16fd118
Smethan <ethanhargus@gmail.com>
2017-07-16 17:14:15 -0500

    Add files via upload

---
+++ BotTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import discord
+import aiohttp
+from discord.ext import commands
+bot = commands.Bot(command_prefix="!")
+class Coolbot:
+	
+	bot.session = aiohttp.ClientSession()
+	Client = discord.Client()
+	bot.load_extension('e926')
+bot.run("MzM0Nzk2ODU2MDQxNDA2NDY0.DEglSw.lO2uWavgkzQnWvarkqk5_P04nQk")
+	
+
+ 
